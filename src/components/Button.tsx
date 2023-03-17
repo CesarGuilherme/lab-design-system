9d1883ecadaed2e9357c6a654f177889c3b2a8a7
César Guilherme Lana Nonato <cesar.nonato@icloud.com>
2023-03-17 14:54:06 -0300

    Criação do formulário

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,21 +1,28 @@
-import { ReactNode } from 'react';
-import { Slot } from '@radix-ui/react-slot';
-import { clsx } from 'clsx';
+import { ButtonHTMLAttributes, ReactNode } from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { clsx } from "clsx";
 
-export interface ButtonProps {
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
   asChild?: boolean;
 }
 
-export function Button({ children, asChild}: ButtonProps) {
-  const Comp = asChild ? Slot : 'button';
-  return(
-    <Comp 
-    className={clsx(
-      'py-4 px-3 bg-gray-10 rounded font-semibold shadow-neo text-black text-sm w-full transition-shadow hover:shadow-neo-flat focus:ring-2 ring-blue-200',
-    )}
+export function Button({
+  children,
+  asChild,
+  className,
+  ...props
+}: ButtonProps) {
+  const Comp = asChild ? Slot : "button";
+  return (
+    <Comp
+      className={clsx(
+        "py-3 px-4 bg-gray-10 rounded font-semibold shadow-neo text-blue-200 text-sm w-full hover:shadow-inner-neo hover:-translate-y-[1px] transition ease-in-out duration-100 outline-none focus:ring-2 ring-blue-200",
+        className
+      )}
+      {...props}
     >
       {children}
     </Comp>
-  )
+  );
 }
--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,13 +1,16 @@
 import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
 import { Check } from "phosphor-react";
 
-export interface CheckboxProps {}
+export interface CheckboxProps extends CheckboxPrimitive.CheckboxProps {}
 
-export function Checkbox({}: CheckboxProps) {
+export function Checkbox(props: CheckboxProps) {
   return (
-    <CheckboxPrimitive.Root className="w-6 h-6 p-[2px] bg-gray-10 rounded shadow-check-neo">
+    <CheckboxPrimitive.Root
+      className="w-6 h-6 p-[2px] bg-gray-10 rounded shadow-check-neo outline-none focus:ring-2 ring-blue-200"
+      {...props}
+    >
       <CheckboxPrimitive.Indicator asChild>
-        <Check weight="bold" className="h-5 w-5 text-blue-100" />
+        <Check weight="bold" className="h-5 w-5 text-blue-200" />
       </CheckboxPrimitive.Indicator>
     </CheckboxPrimitive.Root>
   );
